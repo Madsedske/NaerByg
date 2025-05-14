c3cfa003f5a17b06c73bf7a44b21a7e23b29c6d9
MadsRasmussenStofo <mra@stofo.dk>
2025-05-14 13:52:53 +0200

    ret

--- API/Services/ProductService.cs
+++ API/Services/ProductService.cs
@@ -21,46 +21,46 @@ namespace API.Services
             var products = new List<ProductResponse>();
 
             using var connection = _databaseContext.CreateConnection();
-            using var command = new MySqlCommand("GetProducts", (MySqlConnection)connection)
             {
-                CommandType = CommandType.StoredProcedure
-            };
-                    // Add parameter to avoid SQL injection
-                    command.Parameters.AddWithValue("@search_term", searchTerm);
-                    command.Parameters.AddWithValue("@sys_category_mapping_id", category);
+                using var command = new MySqlCommand("GetProducts", (MySqlConnection)connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                // Add parameter to avoid SQL injection
+                command.Parameters.AddWithValue("@search_term", searchTerm);
+                command.Parameters.AddWithValue("@sys_category_mapping_id", category);
 
-                    // Execute the query
-                    using (var reader = command.ExecuteReader())
+                // Execute the query
+                using (var reader = command.ExecuteReader())
+                {
+                    // Read the results and populate the buttons list
+                    while (reader.Read())
                     {
-                        // Read the results and populate the buttons list
-                        while (reader.Read())
+                        ProductResponse productsResponse = new ProductResponse
                         {
-                            ProductResponse productsResponse = new ProductResponse
-                            {
-                                ProductId = reader.GetInt32("ProductId"),
-                                ProductSku = reader.GetString("ProductSku"),
-                                ProductName = reader.GetString("ProductName"),
-                                Brand = reader.GetString("Brand"),
-                                Price = reader.GetDouble("Price"),
-                                Stock = reader.GetInt32("Stock"),
-                                PictureURL = reader.GetString("PictureURL"),
-                                ChainLogoURL = reader.GetString("ChainLogoURL"),
-                                ShopName = reader.GetString("ShopName"),
-                                ShopAddress = reader.GetString("ShopAddress"),
-                                ShopPostArea = reader.GetString("ShopPostArea"),
-                                ShopCity = reader.GetString("ShopCity"),
-                                ShopPhoneNo = reader.GetString("ShopPhoneNo"),
-                                ShopOpeningHours = reader.GetString("ShopOpeningHours")
-                            };
-                            products.Add(productsResponse);
-                        }
+                            ProductId = reader.GetInt32("ProductId"),
+                            ProductSku = reader.GetString("ProductSku"),
+                            ProductName = reader.GetString("ProductName"),
+                            Brand = reader.GetString("Brand"),
+                            Price = reader.GetDouble("Price"),
+                            Stock = reader.GetInt32("Stock"),
+                            PictureURL = reader.GetString("PictureURL"),
+                            ChainLogoURL = reader.GetString("ChainLogoURL"),
+                            ShopName = reader.GetString("ShopName"),
+                            ShopAddress = reader.GetString("ShopAddress"),
+                            ShopPostArea = reader.GetString("ShopPostArea"),
+                            ShopCity = reader.GetString("ShopCity"),
+                            ShopPhoneNo = reader.GetString("ShopPhoneNo"),
+                            ShopOpeningHours = reader.GetString("ShopOpeningHours")
+                        };
+                        products.Add(productsResponse);
                     }
-                    // Close the connection
-                    connection.Close();
                 }
-                return products;
+                // Close the connection
+                connection.Close();
             }
+            return products;
         }
-    } 
+    }
 }
 
